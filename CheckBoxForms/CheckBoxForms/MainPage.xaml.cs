067fc753de87dcfe777e1daca5399c8b28f0cf01
Abhishek <abhishabd1@outlook.com>
2019-02-08 17:47:15 +0530

    Initial Commit
    Inital Commit

---
+++ CheckBoxForms/CheckBoxForms/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using CheckBoxForms.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CheckBoxForms
+{
+    public partial class MainPage : ContentPage
+    {
+        private ObservableCollection<HelperModel> statusRecords;
+        string[] statusList;
+        public MainPage()
+        {
+            InitializeComponent();
+
+            statusList = new string[] { "All", "red", "green", "yellow", "blue" };
+            statusRecords = new ObservableCollection<HelperModel>();
+
+            //Adding list value into model 
+            foreach (var status in statusList)
+            {
+                statusRecords.Add(new HelperModel
+                {
+                    Name = status
+                });
+            }
+            ListView1.ItemsSource = statusRecords;
+        }
+
+        private void ListView1_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null) return;
+
+            var statusData = e.SelectedItem as HelperModel;
+            ((ListView)sender).SelectedItem = null;
+
+           
+            if (statusData.Name == "All")
+            {
+                statusRecords.ToList().ForEach(x => x.IsSelected = false);
+            }
+            else
+            {
+                statusRecords[0].IsSelected = false;
+            }
+
+            var item = statusRecords.Where(x => x.Name == statusData.Name).FirstOrDefault();
+            if (item != null)
+                item.IsSelected = !item.IsSelected;
+
+            
+        }
+    }
+}
---
+++ CheckBoxForms/CheckBoxForms/ViewModel/HelperModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace CheckBoxForms.ViewModel
+{
+    public class HelperModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private bool isSelected = false;
+
+        public string Name { get; set; }
+
+        public bool IsSelected
+        {
+            get { return isSelected; }
+            set
+            {
+                isSelected = value;
+                OnPropertyChanged("IsSelected");
+            }
+        }
+
+
+        //OnProperty changed method
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
